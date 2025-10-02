6bcb68839c1372ac47d075dd4176ec7c9837d411
UmarIqbalFazal <Umariqbalfazal@outlook.com>
2025-10-02 14:47:35 +0500

    theme and color changes

--- Frontend/src/Components/NoteCard.jsx
+++ Frontend/src/Components/NoteCard.jsx
@@ -20,7 +20,7 @@ const NoteCard = ({ note, setNotes }) => {
   };
 
   return (
-    <div className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]">
+    <div className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-b-4 border-r-4 border-solid border-primary">
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
