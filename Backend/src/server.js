97bd573877ecd419747b5cb07ac2aca4857d67c1
UmarIqbalFazal <Umariqbalfazal@outlook.com>
2025-10-02 18:24:23 +0500

    prepared for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path"
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -9,6 +10,7 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve()
 
 // Connect to MongoDB
 connectDB();
@@ -26,6 +28,14 @@ app.use(express.json());      // Parse JSON request body
 // Routes
 app.use("/api/auth", authRoutes);   // signup & login
 app.use("/api/notes", notesRoutes); // CRUD for notes
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+});
+}
 
 // Start server
 const PORT = process.env.PORT || 5001;
